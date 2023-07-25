91691fcee7c82aa8095470d7cb5b46e4b8c6b15b
Blorpal <nokor9876@gmail.com>
2023-07-25 09:05:50 -0500

    Player Movement completed!

--- game?/main.py
+++ game?/main.py
@@ -1,6 +1,9 @@
 import random
-
+from getch import getch
+import os
 fMap = {}
+
+
 
 #base map no trees, movment etc
 for item in range(1,26):
@@ -15,6 +18,14 @@ for item in fMap:
 #water right side
     for itemy in range(random.randint(22,24),25):
         fMap[item][itemy] = 'w'
+
+#player
+pX = 12
+pY = 13
+fMap[pY][pX] = '@'
+
+
+
 #trees
 treeCount = 0
 for item in fMap:
@@ -28,8 +39,37 @@ for item in fMap:
         if treeCount == 2:
             treeCount = 0
             break
-    
-            
 
-for item in fMap:
-    print(fMap[item])
+
+while True:
+    os.system('clear')
+    
+    for item in fMap:
+        print(fMap[item])
+    print(pY,pX)
+    
+    uInp = getch()
+    
+    if uInp == 'w':
+        if  pY != 1 and fMap[pY-1][pX] == '_':
+            fMap[pY][pX] = '_'
+            pY = pY - 1
+            fMap[pY][pX] = '@'
+
+    if uInp == 's':
+        if  pY != 25 and fMap[pY+1][pX] == '_':
+            fMap[pY][pX] = '_'
+            pY = pY + 1
+            fMap[pY][pX] = '@'
+
+    if uInp == 'd':
+        if pX != 24 and fMap[pY][pX+1] == '_':
+            fMap[pY][pX] = '_'
+            pX = pX + 1
+            fMap[pY][pX] = '@'
+
+    if uInp == 'a':
+        if pX != 0 and fMap[pY][pX-1] == '_':
+            fMap[pY][pX] = '_'
+            pX = pX - 1
+            fMap[pY][pX] = '@'
