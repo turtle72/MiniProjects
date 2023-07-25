c71bea1785469069e192bf787cd29c1cb352f04f
Blorpal <nokor9876@gmail.com>
2023-07-25 10:42:29 -0500

    Added functional trees and inventory system

--- game?/main.py
+++ game?/main.py
@@ -3,7 +3,9 @@ from getch import getch
 import os
 fMap = {}
 
+treeDB = []
 
+inv = []
 
 #base map no trees, movment etc
 for item in range(1,26):
@@ -35,41 +37,83 @@ for item in fMap:
         treeLocal = random.randint(0,24)
         if fMap[item][treeLocal] == '_':
             fMap[item][treeLocal] = 't'
+            treeTuple = (item,treeLocal)
+            treeDB.append(treeTuple)
             treeCount = treeCount + 1
         if treeCount == 2:
             treeCount = 0
             break
 
+notifCode = False
+
+def addItem(type):
+    global notifCode
+    inv.append(type)
+    notifCode = True
+    
+contrDisplay = f"Player cords: X:{pX} Y:{pY} Open Inventory: I"
+contrDisplayNotif = f"Player cords: X:{pX} Y:{pY} Open Inventory: I You picked up an item! enter inv to remove notification."
 
 while True:
+
+#print
     os.system('clear')
-    
+ 
     for item in fMap:
         print(fMap[item])
-    print(pY,pX)
+    if notifCode == True:
+        print(contrDisplayNotif)
+    else:
+        print(contrDisplay)
     
     uInp = getch()
-    
+
+#movement
     if uInp == 'w':
         if  pY != 1 and fMap[pY-1][pX] == '_':
             fMap[pY][pX] = '_'
             pY = pY - 1
             fMap[pY][pX] = '@'
+        elif pY != 1 and fMap[pY-1][pX] == 't':
+            if random.randint(1,15) == 5:
+                addItem("berry")
+
 
     if uInp == 's':
         if  pY != 25 and fMap[pY+1][pX] == '_':
             fMap[pY][pX] = '_'
             pY = pY + 1
             fMap[pY][pX] = '@'
+        elif pY != 25 and fMap[pY+1][pX] == 't':
+            if random.randint(1,15) == 5:
+                addItem("berry")
 
     if uInp == 'd':
         if pX != 24 and fMap[pY][pX+1] == '_':
             fMap[pY][pX] = '_'
             pX = pX + 1
             fMap[pY][pX] = '@'
+        elif pX != 24 and fMap[pY][pX+1] == 't':
+            if random.randint(1,15) == 5:
+                addItem("berry")
 
     if uInp == 'a':
         if pX != 0 and fMap[pY][pX-1] == '_':
             fMap[pY][pX] = '_'
             pX = pX - 1
             fMap[pY][pX] = '@'
+        elif pX != 0 and fMap[pY][pX-1] == 't':
+            if random.randint(1,15) == 5:
+                addItem("berry")
+    
+#other controllers
+
+    if uInp == 'i':
+        while True:
+            os.system('clear')
+            print(f"{inv} press q to leave inv")
+            invInp = getch()
+            
+            if invInp == 'q':
+                notifCode = False
+                break
